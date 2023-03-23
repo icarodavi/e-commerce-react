30bbbd8cad0579e3669d279540af73d58d22a517
Icaro Monteiro <icarodavi@gmail.com>
2023-03-23 14:21:51 -0300

    feat: remove cart context

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,12 @@ import './index.css'
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from './providers/ReactQuery';
 import { ShopProvider } from './context/ShopContext';
-import { CartProvider } from './context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <ShopProvider>
-                <CartProvider>
                     <App />
-                </CartProvider>
             </ShopProvider>
         </QueryClientProvider>
     </React.StrictMode>,
