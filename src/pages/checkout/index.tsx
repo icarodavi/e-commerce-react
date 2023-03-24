b223f4bc696f4277bdcc640ac60db1702d0ea972
Icaro Monteiro <icarodavi@gmail.com>
2023-03-23 22:56:50 -0300

    refact: refact checkout page

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { useContextSelector } from "use-context-selector"
 import { UIButton } from "../../components/UIButton";
 import { ShopContext } from "../../context/ShopContext"
@@ -7,6 +8,7 @@ import { sumItem } from "../../utils/number";
 
 export default function Checkout() {
     const { cart } = useContextSelector(ShopContext, context => context);
+    const navigate = useNavigate();
     return (
         <MainLayout>
             <div className="bg-white">
@@ -221,7 +223,7 @@ export default function Checkout() {
 
 
                                 <div className="mt-10 flex justify-end border-t border-gray-200 pt-6 gap-1">
-                                    <UIButton color="secondary">Cancel</UIButton>
+                                    <UIButton color="secondary" onClick={(e: any)=> {e.preventDefault(); navigate('/cart')}}>Cancel</UIButton>
                                     <button
                                         type="submit"
                                         className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
